c0739ffbd311354f19cad00eab4bf5b3a674528e
Luigi Oliveira <luigijordanio@hotmail.com>
2023-01-31 09:42:58 -0300

    enviropments variable

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const isDevelopment = process.env.NODE_ENV !== "production";
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
+  mode: isDevelopment ? "development" : "production",
   devtool: isDevelopment ? "eval-source-map" : "source-map",
   entry: path.resolve(__dirname, "src", "index.tsx"),
   output: {
